c583c3a551486a37ae104955926ab33b0c123915
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-01-04 19:03:06 -0500

    added logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import CreateCard from "../CreateCard";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div>
       <div> Welcome to the business card creator! </div>
+      <Link to={`/create`}> Visit App </Link>
     </div>
   );
 }
