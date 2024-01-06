faadc22418984d199d02ecb5d2b9831f463ce6dd
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-01-06 12:19:10 -0500

    updated environment

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD_WaEcFpWYnvS4AGFlRT8tWZCRo-r9tEk",
+  apiKey: import.meta.env.API_KEY,
   authDomain: "cardcreate-3113c.firebaseapp.com",
   projectId: "cardcreate-3113c",
   storageBucket: "cardcreate-3113c.appspot.com",
